33b657e960edcada79953fe4b21f2d2d91423b5f
xdnathan <xdnathanm@gmail.com>
2025-04-08 19:02:33 -0600

    Cleaned up le home page and removed all not needed junk

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'nathan-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nathan-app');
+    expect(app.title).toEqual('Pixel Forge')
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nathan-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Pixel Forge');
   });
 });
